d66f813ebee514335330d417edb3ab727a3fe845
Raquel Souza <raquelpds09@gmail.com>
2025-10-20 14:29:45 -0300

    modified ContentView

--- BuyFellings/BuyFeelings/ContentView.swift
+++ BuyFellings/BuyFeelings/ContentView.swift
@@ -6,37 +6,19 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    
-    @ObservedObject var viewModel: ContentViewModel
-    
     var body: some View {
         VStack {
-            Text("Fun duration: \(viewModel.duration)")
-            
-            Button ("Buy Fun duration") {
-                Task {
-                    await viewModel.purchase()
-                }
-            }
-            
-            Divider()
-                .padding(.vertical)
-
-            BuyEmotionsView(
-                title: "Bad Feelings",
-                items: [
-                    .init(id: "com.CaioMandarino.BuyFellings.bad.anxiety", name: "Anxiety", price: 0.99, image: "Anxiety"),
-                    .init(id: "com.CaioMandarino.BuyFellings.good.happiness", name: "Happiness", price: 1.99, image: "Happiness"),
-                    .init(id: "com.CaioMandarino.BuyFellings.good.calm", name: "Companionship", price: 0.49, image: "Calm")
-                ]
-            )
-            .frame(maxWidth: .infinity)
-            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
         .padding()
     }
 }
 
+#Preview {
+    ContentView()
+}
