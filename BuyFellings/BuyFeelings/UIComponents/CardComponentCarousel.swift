de4ea1d5a118dad7ca33612406d1568d0b31cba3
Raquel Souza <raquelpds09@gmail.com>
2025-10-22 18:08:40 -0300

    fixed subscriptions bug

--- BuyFellings/BuyFeelings/UIComponents/CardComponentCarousel.swift
+++ BuyFellings/BuyFeelings/UIComponents/CardComponentCarousel.swift
@@ -128,7 +128,7 @@ struct CardComponentCarousel: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 LazyHStack(spacing: 16) {
                     ForEach(items.filter({ $0.category == .sessionBadFeelings})) { item in
-                        if viewModel.userHavePremium {
+                        if viewModel.userHaveSeason{
                             CardComponent(viewModel: viewModel, item: item)
                                 .frame(width: 340, height: 280)
                                 .padding(.vertical)
@@ -160,7 +160,7 @@ struct CardComponentCarousel: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 LazyHStack(spacing: 16) {
                     ForEach(items.filter({ $0.category == .sessionGoodFeelings})) { item in
-                        if viewModel.userHavePremium {
+                        if viewModel.userHaveSeason {
                             CardComponent(viewModel: viewModel, item: item)
                                 .frame(width: 340, height: 280)
                                 .padding(.vertical)
